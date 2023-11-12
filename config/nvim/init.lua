ebb20777583883b90eedf44b9900455806bfec62
Pradyumna Chatterjee <chatterjee.pra@gmail.com>
2023-11-12 18:37:26 +0530

    Fix nvim tree file issue
    Signed-off-by: Pradyumna Chatterjee <chatterjee.pra@gmail.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -343,12 +343,12 @@ require('packer').startup(function(use)
   use 'mhinz/vim-startify'
   use {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
+    tag = '0.1.4',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use {'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cd build && make' }
   use 'nvim-telescope/telescope-file-browser.nvim'
-  -- use 'LukasPietzschmann/telescope-tabs'
+  use 'LukasPietzschmann/telescope-tabs'
   -- use {
   --   'rmagatti/session-lens',
   --   requires = {'rmagatti/auto-session', 'nvim-telescope/telescope.nvim'}
